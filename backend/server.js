6c8d7816f0103650d162c095b578859a56d89846
asbellrichard429-cpu <asbellrichard429@gmail.com>
2026-05-01 09:35:12 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,6 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-
 const crypto = require('crypto');
 const twilio = require('twilio');
 const db = require('./db');
