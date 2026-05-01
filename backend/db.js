9966a9946b82a979f431b86c82cd2d3561a315da
asbellrichard429-cpu <asbellrichard429@gmail.com>
2026-05-01 16:25:47 -0500

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,1 +1,11 @@
-const {pool } = require('pg');
+require('dotenv') .config();
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectionUnauthorizated: false },
+});
+async function query(text, params) {,
+  const res = await pool.query(text, params);
+  return res;
+}
+module.exports = {query, pool };
